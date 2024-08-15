f6e279c0a33a29ca023c96d6f349517a2ad6f388
klasmer <klasmer495@gmail.com>
2024-08-15 18:02:43 +0300

    Урок 1 Циклы Задача 4

--- src/Main.java
+++ src/Main.java
@@ -17,6 +17,12 @@ public class Main {
         for (int b = 0; b <= 17; b = b + 2) {
             System.out.println("четные числа " + b);
         }
+
+        //Выведите в консоль все числа от 10 до −10 от бо́льшего числа к меньшему.
+        System.out.println("Задача 4");
+        for (int c = 10; c >= -10; c = c - 1) {
+            System.out.println(c);
+        }
     }
 
 }
