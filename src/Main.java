5233276839472287199cfbaacf8594922b14402f
klasmer <klasmer495@gmail.com>
2024-08-15 18:02:08 +0300

    Урок 1 Циклы Задача 2

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,12 @@ public class Main {
         for (int i = 1; i <= 10; i++) {
             System.out.println(i);
         }
+
+        //С помощью цикла for выведите в консоль все целые числа от 10 до 1.
+        System.out.println("Задача 2");
+        for (int a = 10; a >= 1; a = a - 1) {
+            System.out.println(a);
+        }
     }
 
 }
